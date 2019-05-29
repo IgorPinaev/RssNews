e80407854be7323cd3430a3335cd4cc875a83e37
igorpinaev <pinaevigor01@gmail.com>
2019-05-29 09:36:14 +0500

    Initial Commit

---
+++ RssNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RssNews
+//
+//  Created by Игорь Пинаев on 29/05/2019.
+//  Copyright © 2019 Igor Pinaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
